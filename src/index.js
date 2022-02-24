ec3a1233ff9dcf1c01e9f3ec819f135bdd51435f
Louise-Birulia-Bezenson <elizabethbirulia@gmail.com>
2022-02-24 17:21:53 +0300

    finish:task Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let checker = true;
+    while (checker) {
+        checker = false;
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            let indexFromToDelete = str.indexOf(bracketsConfig[i][0] + bracketsConfig[i][1]);
+            if (indexFromToDelete >= 0) {
+                str = str.slice(0, indexFromToDelete) + str.slice(indexFromToDelete + 2);
+                checker = true;
+            }
+        }
+    }
+    if (str.length === 0) {
+        return true;
+    }
+    return false;
 }
